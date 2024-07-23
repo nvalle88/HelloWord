be0e8dfe35dcd5c69ed4f7aeca2e663de2262f48
nvalle88 <30629513+nvalle88@users.noreply.github.com>
2024-07-23 18:13:50 -0500

    Add project files.

---
+++ HelloWord/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics;
+
+namespace HelloWord
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Debug.WriteLine("Hello, World!");
+        }
+    }
+}
